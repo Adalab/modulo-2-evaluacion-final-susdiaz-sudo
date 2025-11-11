6d6d91713eb2f446b4553207a85ad0dd2eea5a4c
Sus.diaz <sus.diazse@gmail.com>
2025-11-11 20:39:05 +0100

    Create the HTML and CSS content and request an array from the API

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
-'use strict';
+"use strict";
 
+// Constantes y variables
 
+let products = [];
+
+const productsList = document.querySelector(".js_productsList");
+
+productsList.innerHTML = products;
+
+// Eventos
+
+// Funciones
+
+fetch("https://raw.githubusercontent.com/Adalab/resources/master/apis/products.json")
+  .then((res) => res.json())
+  .then((data) => {
+    for (let item of data) {
+    productsList.innerHTML += `
+    <li>
+      <img src="${item.image}"></img>
+      <p>${item.title}</p>
+      <p>${item.price} €</p>
+      <button class="purchaseBtn">Comprar</button>
+    </li>
+    `;
+    }
+  });
