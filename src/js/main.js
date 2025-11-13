df8bf908b66a2e57e60270c25f6ff747b4bbb7ff
Sus.diaz <sus.diazse@gmail.com>
2025-11-13 12:40:49 +0100

    Add local storage and functions to add and remove items from the cart

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
-"use strict"
+"use strict";
 
 // Constantes y variables
+
 let allProducts = [];
 let cartProducts = [];
 
@@ -11,15 +12,13 @@ const searchBtn = document.querySelector(".js_searchBtn");
 const inputContent = document.querySelector(".js_inputContent");
 
 const shoppingCart = document.querySelector(".js_shoppingCart");
-const product = document.querySelector(".js_product");
 
 // Funciones
 
 function renderProducts(products) {
-  
   let productsHtml = "";
-  for(let product of products){
-  productsHtml += `
+  for (let product of products) {
+    productsHtml += `
     <li class="js_product" id="${product.id}">
       <img src="${product.image}" alt="${product.title}"></img>
       <p>${product.title}</p>
@@ -31,57 +30,83 @@ function renderProducts(products) {
   return productsHtml;
 }
 
-// Petición al servidor
-fetch(
-  "https://raw.githubusercontent.com/Adalab/resources/master/apis/products.json"
-)
-  .then((res) => res.json())
-  .then((data) => {
-    allProducts = data;
-    productsList.innerHTML = renderProducts(allProducts);
-    return data;
-  });
-
 function addProductCart(productId) {
   const productSelected = allProducts.find((product) => {
-  
     return product.id === parseInt(productId);
   });
   cartProducts.push(productSelected);
-  console.log(cartProducts);
+  refreshLocalStorage();
 
-   shoppingCart.innerHTML = renderProducts(cartProducts);
+  shoppingCart.innerHTML = renderProducts(cartProducts);
 }
 
 function removeProductCart(productId) {
-  const productSelected = allProducts.find((product) => {
-  
-    return product.id === parseInt(productId);
-  });
-  console.log(cartProducts.indexOf(productSelected));
-  
-  cartProducts.splice(cartProducts.indexOf(productSelected), 1);
-  //console.log(cartProducts);
-  
-   shoppingCart.innerHTML = renderProducts(cartProducts);
+  const cartIndex = cartProducts.findIndex(
+    (product) => product.id === parseInt(productId)
+  );
+
+  if (cartIndex !== -1) {
+    cartProducts.splice(cartIndex, 1);
+    refreshLocalStorage();
+  }
+
+  shoppingCart.innerHTML = renderProducts(cartProducts);
 }
 
+function refreshLocalStorage() {
+  localStorage.removeItem("cartLocal");
+  localStorage.removeItem("productsHtmlLocal");
+
+  localStorage.setItem("cartLocal", JSON.stringify(cartProducts));
+  localStorage.setItem("productsHtmlLocal", productsList.innerHTML);
+}
+
+// Petición al servidor
+
+if (localStorage.getItem("productsHtmlLocal") === null) {
+  console.log("Obteniendo datos de servidor");
+
+  fetch(
+    "https://raw.githubusercontent.com/Adalab/resources/master/apis/products.json"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      allProducts = data;
+      productsList.innerHTML = renderProducts(allProducts);
+      localStorage.setItem("allProducts", JSON.stringify(allProducts));
+      localStorage.setItem("productsHtmlLocal", productsList.innerHTML);
+      return data;
+    });
+} else {
+  console.log("Obteniendo datos de localStorage");
+  allProducts = JSON.parse(localStorage.getItem("allProducts"));
+  productsList.innerHTML = localStorage.getItem("productsHtmlLocal");
+}
+
+if (localStorage.getItem("cartLocal") !== null) {
+  console.log("Leyendo carrito de localStorage");
+  cartProducts = JSON.parse(localStorage.getItem("cartLocal"));
+  shoppingCart.innerHTML = renderProducts(cartProducts);
+}
 
 // Eventos
 
 searchBtn.addEventListener("click", (ev) => {
   productsList.innerHTML = "";
   notFoundSpan.classList.add("hidden");
+  let foundProducts = [];
   let results = 0;
   for (let product of allProducts) {
     if (
       product.title.toLowerCase().includes(inputContent.value.toLowerCase())
     ) {
-      productsList.innerHTML += renderProduct(product);
+      foundProducts.push(product);
       results++;
       inputContent.placeholder = "Results: " + results;
     }
   }
+
+  productsList.innerHTML = renderProducts(foundProducts);
 
   if (results === 0) {
     notFoundSpan.classList.remove("hidden");
@@ -100,14 +125,16 @@ productsList.addEventListener("click", (ev) => {
     purchaseBtn.classList.remove("purchaseBtn");
     purchaseBtn.textContent = "Remove";
     productElement.classList.add("productSelected");
+    refreshLocalStorage();
     addProductCart(productElement.id);
-  }else if(clickedElement.classList.contains("deleteBtn")){
+  } else if (clickedElement.classList.contains("deleteBtn")) {
     const productElement = clickedElement.parentNode;
     const purchaseBtn = clickedElement;
     purchaseBtn.classList.remove("deleteBtn");
     purchaseBtn.classList.add("purchaseBtn");
     purchaseBtn.textContent = "Buy";
     productElement.classList.remove("productSelected");
+    refreshLocalStorage();
     removeProductCart(productElement.id);
   }
 });
